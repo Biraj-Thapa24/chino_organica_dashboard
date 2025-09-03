9a135e0b1e5b326d83e35a6946d5868c4537e1ac
Biraj Thapa <birajthapa983@gmail.com>
2025-09-03 15:54:59 +0545

    vite & tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-green-300 underline px-5 py-3 rounder">
+      Chino Organica
+    </div>
+  );
+};
+
+export default App;
