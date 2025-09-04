f92a1228d48eb46a181e79cb1a904c8c1826f370
Nitu-kush <frontendint05@webstudionepal.com>
2025-09-04 10:10:28 +0545

    changes in the app.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="bg-green-300 underline px-5 py-3 rounder">
-      Chino Organica testing from nitu
+      Chino Organica testing from nitu hello there
     </div>
   );
 };
